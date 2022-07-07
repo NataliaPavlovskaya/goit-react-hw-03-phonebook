d21a429bd78d737431c01d73b11df448c4721a8b
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-07-07 15:43:24 +0300

    yw 3 - task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,94 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import ContactForm from "./ContactForm/ContactForm";
+import initialContacts from "../contacts.json"
+
+
+export default class App extends Component {
+  state = {
+    contacts: initialContacts,
+    filter: "",
+  };
+
+  addContact = (task) => {
+    const searchSameName = this.state.contacts
+      .map((cont) => cont.name)
+      .includes(task.name);
+
+    if (searchSameName) {
+      alert(`${task.name} is already in contacts`);
+    } else if (task.name.length === 0) {
+      alert("Fields must be filled!");
+    } else {
+      const contact = {
+        ...task,
+        id: uuidv4(),
+      };
+
+      this.setState((prevState) => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    }
+  };
+
+  changeFilter = (filter) => {
+    this.setState({ filter });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter((contacts) =>
+      contacts.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  removeContact = (contactId) => {
+    this.setState((prevState) => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+  componentDidMount(){
+   const contacts = localStorage.getItem('contacts');
+   const parsedContacts = JSON.parse(contacts);
+
+   if(parsedContacts){
+    this.setState({contacts: parsedContacts});
+   }
+   
+  }
+  componentDidUpdate(prevProps,prevState){
+
+    if (this.state.contacts !==prevState.contacts){
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const { filter } = this.state;
+
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+
+        <ContactForm onAddContact={this.addContact} />
+        <h2>Contacts</h2>
+        {visibleContacts.length > 1 && (
+          <Filter value={filter} onChangeFilter={this.changeFilter} />
+        )}
+        {visibleContacts.length > 0 && (
+          <ContactList
+            contacts={visibleContacts}
+            onRemoveContact={this.removeContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
